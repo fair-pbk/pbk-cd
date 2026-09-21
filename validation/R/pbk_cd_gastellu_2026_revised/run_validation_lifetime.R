71115f4f7265881dc94884e7a5f0edb4ad7b804e
Jordi Minnema <minnemj@rivm-comp-l18p.rivm.ssc-campus.nl>
2026-09-21 13:34:23 +0200

    Added validation scripts for revised mR-model. Added Pouillot validation for original - model. Updated R.yaml to include proper comparison with SBML model.

---
+++ validation/R/pbk_cd_gastellu_2026_revised/run_validation_lifetime.R
@@ -0,0 +1,140 @@
+#-------------------------------------------------------------------------------
+# Single dose scenario : simulation of 40 days for single adult age 30 with
+# default parameters and single dose of 1 mg at time 0.
+#-------------------------------------------------------------------------------
+
+# Load packages
+library(rxode2)
+library(dplyr)
+source("validation/R/pbk_cd_gastellu_2026_revised/Cd_Parameters_CV.R")
+source("validation/R/pbk_cd_gastellu_2026_revised/Time varying inputs and assignment rules_CV.R")
+source("validation/R/pbk_cd_gastellu_2026_revised/cd_pbk_shared_without source tracking_mod_v3_CV.R")
+
+
+
+# /Needsrxode2# /Needs to be adjusted!!!!!!!!
+
+# Simulation setup
+
+sex_i <- 2
+Delta_BWs3 <- 1
+Delta_creat <- 1
+
+age_start <- 0*365 # years
+age_end <- 10*365
+dosing_stepsize <- 1
+observation_times <- seq(age_start, age_end, by = 1)
+
+# Pre-compute physiology covariates
+phys_cov <- do.call(rbind, lapply(observation_times, function(t) {
+  
+  p <- phys(
+    time = t,
+    sex = sex_i,
+    Delta_BWs3 = Delta_BWs3,
+    Delta_creat = Delta_creat,
+    k5_h = unname(theta["k5_h"]),
+    k5_f = unname(theta["k5_f"]),
+    k17  = unname(theta["k17"]),
+    k19  = unname(theta["k19"]),
+    k21  = unname(theta["k21"])
+  )
+  
+  data.frame(
+    id = 1,
+    time = t,
+    wbw_f = p$wbw_f,
+    vb = p$vb,
+    vk = p$vk,
+    VInhalation = p$VInhalation,
+    Vurine = p$Vurine,
+    ucr = p$ucr,
+    k5 = p$k5,
+    k17x = p$k17x,
+    k17b = p$k17b,
+    k19x = p$k19x
+  )
+}))
+
+# Initial physiology at birth
+p0 <- phys(
+  time = 0,
+  sex = sex_i,
+  Delta_BWs3 = Delta_BWs3,
+  Delta_creat = Delta_creat,
+  k5_h = unname(theta["k5_h"]),
+  k5_f = unname(theta["k5_f"]),
+  k17  = unname(theta["k17"]),
+  k19  = unname(theta["k19"]),
+  k21  = unname(theta["k21"])
+)
+
+# Initial states
+inits <- c(
+  DIET_ing = 0,
+  AIR_inhal_ing = 0,
+  CIG_inhal_ing = 0,
+  SOIL_ing = 0,
+  DUST_ing = 0,
+  COSM_ing = 0,
+  AIR_derm = 0,
+  DUST_derm = 0,
+  COSM_derm = 0,
+  LUNG = 0,
+  GUT = 0,
+  INTESTINE = 0,
+  UPTAKE1 = 0,
+  PLASMA = 0,
+  RBC = 0,
+  META = 0,
+  LIVER = 0,
+  KIDNEY = 0,
+  OTHER = 0,
+  FECES = 0,
+  URINE = 0,
+  EXH = 0
+)
+
+ev_lifetime <- data.frame(
+  id = 1,
+  time = seq(age_start,age_end,dosing_stepsize),
+  evid = 1,
+  cmt = "GUT"
+)
+
+# Add observation rows before joining physiology so rxSolve receives a plain
+# data frame with all required covariates.
+ev_observed <- data.frame(
+  id = 1,
+  time = observation_times-age_start,
+  evid = 0,
+  cmt = NA_character_,
+  amt = 0
+)
+
+phys_cov$time <- observation_times-age_start
+event_res <- ev_lifetime %>%
+  bind_rows(ev_observed) %>%
+  left_join(phys_cov, by = c("id", "time")) %>%
+  mutate(amt=0.2*dosing_stepsize*wbw_f) %>%
+  arrange(id, time, desc(evid))
+
+
+# Solve
+sim_output <- rxSolve(
+  PBK1,
+  params = theta,
+  events = event_res,
+  inits = inits
+) 
+
+# Write outputs
+model_id <- "pbk_cd_gastellu_2026_revised"
+results_path <- "validation/outputs/reference/R/oral_repeated_lifetime"
+
+## Create results path if not exists
+if (!dir.exists(file.path(results_path))) {
+  dir.create(file.path(results_path), recursive = TRUE)
+}
+
+write.csv(sim_output, paste(results_path, "/", model_id, ".csv", sep=""))
---
+++ validation/R/pbk_cd_gastellu_2026_revised/run_validation_repeated.R
@@ -0,0 +1,143 @@
+#-------------------------------------------------------------------------------
+# Single dose scenario : simulation of 40 days for single adult age 30 with
+# default parameters and single dose of 1 mg at time 0.
+#-------------------------------------------------------------------------------
+
+# Load packages
+library(rxode2)
+library(dplyr)
+source("validation/R/pbk_cd_gastellu_2026_revised/Cd_Parameters_CV.R")
+source("validation/R/pbk_cd_gastellu_2026_revised/Time varying inputs and assignment rules_CV.R")
+source("validation/R/pbk_cd_gastellu_2026_revised/cd_pbk_shared_without source tracking_mod_v3_CV.R")
+
+
+
+# /Needsrxode2# /Needs to be adjusted!!!!!!!!
+
+# Simulation setup
+
+sex_i <- 1
+Delta_BWs3 <- 1
+Delta_creat <- 1
+
+ndays <- 40 # days
+age_start <- 30 # years
+age_start_days <- age_start * 365
+times <- seq(age_start_days, age_start_days + ndays, by = 1)
+
+# Pre-compute physiology covariates
+phys_cov <- do.call(rbind, lapply(times, function(t) {
+  
+  p <- phys(
+    time = t,
+    sex = sex_i,
+    Delta_BWs3 = Delta_BWs3,
+    Delta_creat = Delta_creat,
+    k5_h = unname(theta["k5_h"]),
+    k5_f = unname(theta["k5_f"]),
+    k17  = unname(theta["k17"]),
+    k19  = unname(theta["k19"]),
+    k21  = unname(theta["k21"])
+  )
+  
+  data.frame(
+    id = 1,
+    time = t,
+    wbw_f = p$wbw_f,
+    vb = p$vb,
+    vk = p$vk,
+    VInhalation = p$VInhalation,
+    Vurine = p$Vurine,
+    ucr = p$ucr,
+    k5 = p$k5,
+    k17x = p$k17x,
+    k17b = p$k17b,
+    k19x = p$k19x
+  )
+}))
+
+# Initial physiology at birth
+p0 <- phys(
+  time = 0,
+  sex = sex_i,
+  Delta_BWs3 = Delta_BWs3,
+  Delta_creat = Delta_creat,
+  k5_h = unname(theta["k5_h"]),
+  k5_f = unname(theta["k5_f"]),
+  k17  = unname(theta["k17"]),
+  k19  = unname(theta["k19"]),
+  k21  = unname(theta["k21"])
+)
+
+# Initial states
+inits <- c(
+  DIET_ing = 0,
+  AIR_inhal_ing = 0,
+  CIG_inhal_ing = 0,
+  SOIL_ing = 0,
+  DUST_ing = 0,
+  COSM_ing = 0,
+  AIR_derm = 0,
+  DUST_derm = 0,
+  COSM_derm = 0,
+  LUNG = 0,
+  GUT = 0,
+  INTESTINE = 0,
+  UPTAKE1 = 0,
+  PLASMA = 0,
+  RBC = 0,
+  META = 0,
+  LIVER = 0,
+  KIDNEY = 0,
+  OTHER = 0,
+  FECES = 0,
+  URINE = 0,
+  EXH = 0
+)
+
+
+
+ev_repeated <- data.frame(
+  id = 1,
+  time = seq(0, ndays, by = 1),
+  evid = 1,
+  cmt = "GUT",
+  amt = 100
+)
+
+# Add observation rows separately so each dose time is also sampled.
+ev_observed <- data.frame(
+  id = 1,
+  time = seq(0, ndays, by = 1),
+  evid = 0,
+  cmt = NA_character_,
+  amt = 0
+)
+
+ev_repeated <- bind_rows(ev_repeated, ev_observed)
+
+# Add physiology covariates to every event and observation row
+phys_cov$time <- phys_cov$time - age_start_days
+ev_repeated <- ev_repeated %>%
+  left_join(phys_cov, by = c("id", "time")) %>%
+  arrange(id, time, desc(evid))
+
+# Solve
+sim_output <- rxSolve(
+  PBK1,
+  params = theta,
+  events = ev_repeated,
+  inits = inits
+) %>%
+  dplyr::filter(time %in% seq(0,ndays, 1)) 
+
+# Write outputs
+model_id <- "pbk_cd_gastellu_2026_revised"
+results_path <- "validation/outputs/reference/R/oral_repeated"
+
+## Create results path if not exists
+if (!dir.exists(file.path(results_path))) {
+  dir.create(file.path(results_path), recursive = TRUE)
+}
+
+write.csv(sim_output, paste(results_path, "/", model_id, ".csv", sep=""))
